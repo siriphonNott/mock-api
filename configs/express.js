8e76ee49c12dd91cb5c8138942fada956a58a0d2
nottdev <siriphonnot@gmail.com>
2020-10-08 14:54:03 +0700

    Fix database config

--- configs/express.js
+++ configs/express.js
@@ -1,31 +1,28 @@
-const express  = require('express'),
-      morgan   = require('morgan'),
-      cors     = require('cors')
-      passport = require("passport"),
-      path     = require('path')
+const express = require('express'),
+  morgan = require('morgan'),
+  cors = require('cors')
+;(passport = require('passport')), (path = require('path'))
 
 module.exports = async (app) => {
-  
   // Connect MongoDB
-  require('../configs/databases')
+  // require('../configs/databases')
 
   // CORS
   app.use(cors())
 
   // Parser Body
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
 
   // Logger
   app.use(morgan('dev'))
 
   // Passport
-  require('../configs/passport');
+  require('../configs/passport')
 
   // Static file
   app.use('/static', express.static(path.join(__dirname, '../public')))
 
   // Custom Response Format
   app.use(require('../configs/responseFormat'))
-  
 }
